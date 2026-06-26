6d5a2ce6e2ff1c3cb7f1cea605da1ff6bf752432
Rushil <rushilbti@gmail.com>
2026-06-26 12:37:05 +0530

    Logo update

--- app/manifest.ts
+++ app/manifest.ts
@@ -12,13 +12,13 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#000000",
     icons: [
       {
-        src: "/favicon.ico",
-        sizes: "64x64",
+        src: "/logo.png",
+        sizes: "192x192",
         type: "image/png",
       },
       {
-        src: "/favicon.ico",
-        sizes: "64x64",
+        src: "/logo.png",
+        sizes: "512x512",
         type: "image/png",
         purpose: "maskable",
       },
